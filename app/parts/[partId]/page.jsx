502f703bb83d7a9a929da6eda8cc0538f9c7de75
graslowsnail <graslowsnail@gmail.com>
2024-01-31 04:50:56 -0600

    added single card component to handle fetching the part data and show part info

--- app/parts/[partId]/page.jsx
+++ app/parts/[partId]/page.jsx
@@ -1,42 +1,14 @@
-'use client'
 // app/parts/partId
-import React, { useState, useEffect } from 'react';
+import { SinglePartCard } from '@/components';
 
 const SinglePartPage = ({ params }) => {
   const { partId } = params;
   //console.log(partId);
-  const [part, setPart] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`http://localhost:3000/api/parts/${partId}`);
-        const data = await response.json();
-        console.log(data);
-        setPart(data.part);
-      } 
-      catch(error) {
-        console.error('there was an error fetching the data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
 
   return (
-    <li className="part-item">
-      <div className="part-item__content">
-        <div className="part-item__info">
-          <h2>{part.name}</h2>
-          <h3>PART SIZE: {part.size}</h3>
-          <h3>Box quantity: {part.boxQuantity}</h3>
-          <h3>CURRENT STOCK: {part.currentStock}</h3>
-          <h3>Initial Stock: {part.initialStock}</h3>
-          <h3>BARCODE ID: {part.barCodeId}</h3>
-        </div>
-      </div>
-    </li>
+    <div>
+     < SinglePartCard partId={partId}/>
+    </div>
   );
 };
 
--- components/index.jsx
+++ components/index.jsx
@@ -1,7 +1,9 @@
 import Header from './Header';
 import PartsList from './PartsList';
+import SinglePartCard from './SinglePartCard';
 
 export {
   Header,
   PartsList,
+  SinglePartCard,
 };
